72dfadc31cfbf74a94b730409bdbc36f3ae28834
SirKarib <76648710+SirKarib@users.noreply.github.com>
2021-04-03 14:43:03 +0400

    some changes

--- csv_preprocessing.py
+++ csv_preprocessing.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('export_file.csv', newline='') as File:
+with open('export_file.csv', encoding="UTF-8", newline='') as File:
     reader = csv.reader(File)
     for row in reader:
         print(row)
