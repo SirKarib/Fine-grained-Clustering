2e992084418995331e0b94fbaf27cbc35ab27cc9
SirKarib <76648710+SirKarib@users.noreply.github.com>
2021-04-04 15:33:56 +0400

    some changes

--- edit_review.py
+++ edit_review.py
@@ -12,13 +12,16 @@ morph = pymorphy2.MorphAnalyzer()
 text = text.lower()
 string.punctuation  # переведём символы в единый регистр
 spec_chars = string.punctuation + '\n\xa0«»\t—…'  # Набор специальных символов, которые будут удалены из текста
-text = "".join([ch for ch in text if ch not in spec_chars])  # Для удаления символов используем поэлементную обработку строки
-                                                             # разделим исходную строку text на символы, оставим только символы,
-                                                             # не входящие в набор spec_chars и снова объединим список символов в строку
+text = "".join([ch for ch in text if ch not in spec_chars])
+# Для удаления символов используем поэлементную обработку строки
+# разделим исходную строку text на символы, оставим только символы,
+# не входящие в набор spec_chars и снова объединим список символов в строку
+
 text_tokens = word_tokenize(text)   # text_tokens представляет собой список слов (токенов)
 print(text_tokens, type(text_tokens), len(text_tokens))
 
-text = nltk.Text(text_tokens)  # Для применения инструментов частотного анализа библиотеки NLTK необходимо список токенов преобразовать к классу Text
+text = nltk.Text(text_tokens)
+# Для применения инструментов частотного анализа библиотеки NLTK необходимо список токенов преобразовать к классу Text
 
 print(type(text))
 
