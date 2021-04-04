062095c0d29df4bba82e957ea601112b9a726111
SirKarib <76648710+SirKarib@users.noreply.github.com>
2021-04-04 15:35:15 +0400

    some changes

--- csv_preprocessing.py
+++ csv_preprocessing.py
@@ -1,4 +1,12 @@
 import csv
+import string
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+import zipfile
+import pymorphy2
+
 
 with open('export_file.csv', encoding="UTF-8", newline='') as File:
     reader = csv.reader(File)
